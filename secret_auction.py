ce6890f6ba4314ccd77e4538022dd8c64740a5c9
Klay Clarke <klayaclarke@gmail.com>
2021-11-08 23:30:17 -0500

    create command line app that allows users to bid with discretion .. returns highest bidder once all bids have been recieved

---
+++ secret_auction.py
@@ -0,0 +1,22 @@
+import os
+
+cls = lambda: os.system('cls')
+
+more_bidders = True
+
+names_n_bids = {}
+
+while more_bidders:
+    name = input('What is your name?')
+    bid = int(input('What is your bid?'))
+    if bool(names_n_bids) == False or int(names_n_bids['bid']) < bid:
+        names_n_bids['name'] = name
+        names_n_bids['bid'] = bid
+    elif int(names_n_bids['bid']) > bid:
+        names_n_bids = names_n_bids
+    ask_if_more = input('Are there more bidders? Type either \'yes\' or\' no\'\n').lower()
+    print(names_n_bids)
+    if ask_if_more == 'yes':
+      cls()
+    elif ask_if_more == 'no':
+      more_bidders = False
